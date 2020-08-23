cb325b0634c8ceee99bcbc6a0e4cb0c0a8061d25
John Berger <jxberger@hotmail.com>
2020-08-23 11:41:22 -0600

    workig on the data output from the JSON call

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ $(function () {
             url: "https://api.giphy.com/v1/gifs/search?api_key=OOUmhUZ3zWjK8McmjN8ge20wUweRCUXD&q=FJ Cruiser&limit=5&offset=0&rating=g&lang=en",
             success: function ( data) {
                 console.log( data)
+                $('#outputArea').html(data);
             }
         });
-        $('#outputArea').html( data);
+        
     });
 
 });
