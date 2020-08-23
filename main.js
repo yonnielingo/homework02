0d0aa9caa40ff7d2070c2478119ac4bc7219767f
John Berger <jxberger@hotmail.com>
2020-08-23 17:24:33 -0600

    set up url as a var and elements of url as vars.

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-$(function () {
-    $('#submitSearch').on('click', function () {
+$(document).ready(function () {
+    $("#submitSearch").click(function () {
+
         // get the value from the search content box
-        // var searchQ = $('#searchBox').val();
-        // alert(searchQ);
+        var apiKey = 'OOUmhUZ3zWjK8McmjN8ge20wUweRCUXD';
+        var searchFor = $('#searchBox').val();
+        var limitReturn = 5;
+        var offsetNum = 0;
+        var rating = "g";
+        var defaultLang = "en";
+        var url ='https://api.giphy.com/v1/gifs/search?api_key='+ apiKey +'&q='+ searchFor +'&limit='+ limitReturn +'&offset=0&rating='+ rating +'&lang='+ defaultLang;
+        //alert(url);
 
         //api call to get images
-        $.ajax({
-            type: "GET",
-            url: "https://api.giphy.com/v1/gifs/search?api_key=OOUmhUZ3zWjK8McmjN8ge20wUweRCUXD&q=FJ Cruiser&limit=5&offset=0&rating=g&lang=en",
-            success: function ( data) {
-                console.log( data)
-                $('#outputArea').html(data);
-            }
-        });
-        
+        // $.ajax({
+        //     type: "GET",
+        //     url: url,
+        //     success: function ( data) {
+        //         $("#outputArea").text(data);
+        //     }
+        // });
     });
-
 });
