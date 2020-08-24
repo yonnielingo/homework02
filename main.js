c1d0d87cd2d1a34c83ec168c2e6ca432ec01a941
John Berger <jxberger@hotmail.com>
2020-08-23 20:52:23 -0600

    Going back to the ajax method. getJSON, not wrkng

--- main.js
+++ main.js
@@ -10,20 +10,17 @@ $(document).ready(function () {
         var defaultLang = "en";
         var url ='https://api.giphy.com/v1/gifs/search?api_key='+ apiKey +'&q='+ searchFor +'&limit='+ limitReturn +'&offset=0&rating='+ rating +'&lang='+ defaultLang;
         //alert(url);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        var str = "";
+        $.getJSON( url,function(data ) {
+            console.log(data)
+            var myData = data;
+            $.each(data, function( myData, obj) {
+                str += '<div class="box"><img src="' + obj.url + '" alt="'+ obj.slug + '"/></div>\n';
+                console.log('str='+str)
+            })
+            console.log('str:\n'+str);
+            $('#outputArea').html(str);
+        } )
 
 
         //earlier attempt:
