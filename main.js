f719bbafa04cc7e1fbc0cb2c53e1de253f9ef106
John Berger <jxberger@hotmail.com>
2020-08-25 11:06:04 -0600

    added more input options.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var searchFor = $('#searchBox').val();
         var limitReturn = $('#returnQty').val();
         var offsetNum = 0;
-        var rating = 'g';
+        var rating = $('#raitings').val;
         var defaultLang = 'en';
         var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + searchFor + '&limit=' + limitReturn + '&offset=' + offsetNum + '&rating=' + rating + '&lang=' + defaultLang;
 
@@ -17,12 +17,12 @@ $(document).ready(function () {
                 console.log('success ' + data)
                 var newLine = "";
                 $.each(data.data, function (key, obj) {
-                    var ibArray = obj.images.original.url;
+                    var ibArray = obj.images.downsized_still.url;
                     console.log('url= ' + ibArray);
                     newLine += '<img class="gifs" src="' + ibArray + '"></div>'
                 });
                 $('#outputArea').html(newLine);
-            }
+            },
         });
     });
 });
